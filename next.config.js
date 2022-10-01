b5fb1be7abe3f575799ab3d8d98ecaf59e8dc716
Islam <islamyehia1999@gmail.com>
2022-10-01 22:36:51 +0200

    Using React instead of Preact

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+  productionBrowserSourceMaps: false,
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat",
+  //     });
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
